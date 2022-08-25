aafe7ba1b5a24d946db877f4c6c390c63adbca2b
RackelFonseca <rackelnaganay@gmail.com>
2022-08-25 19:17:07 -0300

    Adicionar arquivos de projeto.

---
+++ aula arquivo/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace aula_arquivo
+{
+    partial class AGENDA
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AGENDA));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtnome = new System.Windows.Forms.TextBox();
+            this.txtelefone = new System.Windows.Forms.TextBox();
+            this.txtendereco = new System.Windows.Forms.TextBox();
+            this.txtcaminhoorigem = new System.Windows.Forms.TextBox();
+            this.txtcaminhodestino = new System.Windows.Forms.TextBox();
+            this.txtnomearquivo = new System.Windows.Forms.TextBox();
+            this.bntexecutar = new System.Windows.Forms.Button();
+            this.bntadicionar = new System.Windows.Forms.Button();
+            this.lblista = new System.Windows.Forms.ListBox();
+            this.rbsalva = new System.Windows.Forms.RadioButton();
+            this.rbapaga = new System.Windows.Forms.RadioButton();
+            this.rbmove = new System.Windows.Forms.RadioButton();
+            this.rbcopia = new System.Windows.Forms.RadioButton();
+            this.btnlimpa = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "telefone";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "endereco";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "caminho origem";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(22, 186);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "caminho destino";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(22, 224);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "nome arquivo";
+            // 
+            // txtnome
+            // 
+            this.txtnome.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtnome.Location = new System.Drawing.Point(61, 36);
+            this.txtnome.Name = "txtnome";
+            this.txtnome.Size = new System.Drawing.Size(291, 20);
+            this.txtnome.TabIndex = 0;
+            // 
+            // txtelefone
+            // 
+            this.txtelefone.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtelefone.Location = new System.Drawing.Point(66, 67);
+            this.txtelefone.Name = "txtelefone";
+            this.txtelefone.Size = new System.Drawing.Size(286, 20);
+            this.txtelefone.TabIndex = 1;
+            // 
+            // txtendereco
+            // 
+            this.txtendereco.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtendereco.Location = new System.Drawing.Point(73, 103);
+            this.txtendereco.Name = "txtendereco";
+            this.txtendereco.Size = new System.Drawing.Size(279, 20);
+            this.txtendereco.TabIndex = 2;
+            // 
+            // txtcaminhoorigem
+            // 
+            this.txtcaminhoorigem.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtcaminhoorigem.Location = new System.Drawing.Point(109, 148);
+            this.txtcaminhoorigem.Name = "txtcaminhoorigem";
+            this.txtcaminhoorigem.Size = new System.Drawing.Size(243, 20);
+            this.txtcaminhoorigem.TabIndex = 3;
+            // 
+            // txtcaminhodestino
+            // 
+            this.txtcaminhodestino.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtcaminhodestino.Location = new System.Drawing.Point(109, 183);
+            this.txtcaminhodestino.Name = "txtcaminhodestino";
+            this.txtcaminhodestino.Size = new System.Drawing.Size(243, 20);
+            this.txtcaminhodestino.TabIndex = 4;
+            // 
+            // txtnomearquivo
+            // 
+            this.txtnomearquivo.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtnomearquivo.Location = new System.Drawing.Point(109, 221);
+            this.txtnomearquivo.Name = "txtnomearquivo";
+            this.txtnomearquivo.Size = new System.Drawing.Size(243, 20);
+            this.txtnomearquivo.TabIndex = 5;
+            // 
+            // bntexecutar
+            // 
+            this.bntexecutar.BackColor = System.Drawing.Color.Red;
+            this.bntexecutar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bntexecutar.Location = new System.Drawing.Point(138, 274);
+            this.bntexecutar.Name = "bntexecutar";
+            this.bntexecutar.Size = new System.Drawing.Size(141, 52);
+            this.bntexecutar.TabIndex = 12;
+            this.bntexecutar.Text = "executar";
+            this.bntexecutar.UseVisualStyleBackColor = false;
+            this.bntexecutar.Click += new System.EventHandler(this.bntexecutar_Click);
+            // 
+            // bntadicionar
+            // 
+            this.bntadicionar.BackColor = System.Drawing.Color.Red;
+            this.bntadicionar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bntadicionar.Location = new System.Drawing.Point(138, 332);
+            this.bntadicionar.Name = "bntadicionar";
+            this.bntadicionar.Size = new System.Drawing.Size(141, 56);
+            this.bntadicionar.TabIndex = 13;
+            this.bntadicionar.Text = "adicionar";
+            this.bntadicionar.UseVisualStyleBackColor = false;
+            this.bntadicionar.Click += new System.EventHandler(this.bntadicionar_Click);
+            // 
+            // lblista
+            // 
+            this.lblista.FormattingEnabled = true;
+            this.lblista.Location = new System.Drawing.Point(370, 36);
+            this.lblista.Name = "lblista";
+            this.lblista.Size = new System.Drawing.Size(273, 407);
+            this.lblista.TabIndex = 14;
+            // 
+            // rbsalva
+            // 
+            this.rbsalva.AutoSize = true;
+            this.rbsalva.Location = new System.Drawing.Point(33, 414);
+            this.rbsalva.Name = "rbsalva";
+            this.rbsalva.Size = new System.Drawing.Size(94, 17);
+            this.rbsalva.TabIndex = 15;
+            this.rbsalva.TabStop = true;
+            this.rbsalva.Text = " salvar arquivo";
+            this.rbsalva.UseVisualStyleBackColor = true;
+            // 
+            // rbapaga
+            // 
+            this.rbapaga.AutoSize = true;
+            this.rbapaga.Location = new System.Drawing.Point(33, 446);
+            this.rbapaga.Name = "rbapaga";
+            this.rbapaga.Size = new System.Drawing.Size(99, 17);
+            this.rbapaga.TabIndex = 16;
+            this.rbapaga.TabStop = true;
+            this.rbapaga.Text = " apagar arquivo";
+            this.rbapaga.UseVisualStyleBackColor = true;
+            // 
+            // rbmove
+            // 
+            this.rbmove.AutoSize = true;
+            this.rbmove.Location = new System.Drawing.Point(148, 446);
+            this.rbmove.Name = "rbmove";
+            this.rbmove.Size = new System.Drawing.Size(92, 17);
+            this.rbmove.TabIndex = 17;
+            this.rbmove.TabStop = true;
+            this.rbmove.Text = "mover arquivo";
+            this.rbmove.UseVisualStyleBackColor = true;
+            this.rbmove.CheckedChanged += new System.EventHandler(this.rbmove_CheckedChanged);
+            // 
+            // rbcopia
+            // 
+            this.rbcopia.AutoSize = true;
+            this.rbcopia.Location = new System.Drawing.Point(148, 414);
+            this.rbcopia.Name = "rbcopia";
+            this.rbcopia.Size = new System.Drawing.Size(92, 17);
+            this.rbcopia.TabIndex = 18;
+            this.rbcopia.TabStop = true;
+            this.rbcopia.Text = "copiar arquivo";
+            this.rbcopia.UseVisualStyleBackColor = true;
+            // 
+            // btnlimpa
+            // 
+            this.btnlimpa.Location = new System.Drawing.Point(557, 459);
+            this.btnlimpa.Name = "btnlimpa";
+            this.btnlimpa.Size = new System.Drawing.Size(75, 23);
+            this.btnlimpa.TabIndex = 19;
+            this.btnlimpa.Text = "limpartexto";
+            this.btnlimpa.UseVisualStyleBackColor = true;
+            this.btnlimpa.Click += new System.EventHandler(this.btnlimpa_Click);
+            // 
+            // AGENDA
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(655, 547);
+            this.Controls.Add(this.btnlimpa);
+            this.Controls.Add(this.rbcopia);
+            this.Controls.Add(this.rbmove);
+            this.Controls.Add(this.rbapaga);
+            this.Controls.Add(this.rbsalva);
+            this.Controls.Add(this.lblista);
+            this.Controls.Add(this.bntadicionar);
+            this.Controls.Add(this.bntexecutar);
+            this.Controls.Add(this.txtnomearquivo);
+            this.Controls.Add(this.txtcaminhodestino);
+            this.Controls.Add(this.txtcaminhoorigem);
+            this.Controls.Add(this.txtendereco);
+            this.Controls.Add(this.txtelefone);
+            this.Controls.Add(this.txtnome);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Name = "AGENDA";
+            this.Text = "AGENDA";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtnome;
+        private System.Windows.Forms.TextBox txtelefone;
+        private System.Windows.Forms.TextBox txtendereco;
+        private System.Windows.Forms.TextBox txtcaminhoorigem;
+        private System.Windows.Forms.TextBox txtcaminhodestino;
+        private System.Windows.Forms.TextBox txtnomearquivo;
+        private System.Windows.Forms.Button bntexecutar;
+        private System.Windows.Forms.Button bntadicionar;
+        private System.Windows.Forms.ListBox lblista;
+        private System.Windows.Forms.RadioButton rbsalva;
+        private System.Windows.Forms.RadioButton rbapaga;
+        private System.Windows.Forms.RadioButton rbmove;
+        private System.Windows.Forms.RadioButton rbcopia;
+        private System.Windows.Forms.Button btnlimpa;
+    }
+}
+
---
+++ aula arquivo/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace aula_arquivo
+{
+    public partial class AGENDA : Form
+    {
+        string nomearquivo = "aluno.txt";
+        string caminhoorigem= "C:/rackel/";
+        string caminhodestino = " D:/Rackel/caminhodestino";
+        public AGENDA()
+
+
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtnomearquivo.Text = nomearquivo;
+            txtcaminhoorigem.Text =caminhoorigem;
+            txtcaminhodestino.Text = caminhodestino;
+        }
+
+        private void bntadicionar_Click(object sender, EventArgs e)
+        {
+            lblista.Items.Add(txtnome.Text.Trim());//adicionando o nome a lista
+            lblista.Items.Add(txtelefone.Text.Trim());
+            lblista.Items.Add(txtendereco.Text.Trim());
+            lblista.Items.Add("===========================");
+
+            txtnome.Text = "";
+            txtendereco.Text = "";
+            txtelefone.Text = "";
+
+            txtnome.Focus();
+        }
+        //botam executar
+        private void bntexecutar_Click(object sender, EventArgs e)
+        {
+            //salva
+            if (rbsalva.Checked == true)
+            {
+                StreamWriter texto = new StreamWriter(caminhoorigem + nomearquivo);
+
+                for (int i = 0; i < lblista.Items.Count; i++)
+                {
+
+                    texto.WriteLine(lblista.Items[i].ToString());
+
+                }
+                texto.Close();
+                MessageBox.Show("arquivo gerado com sucesso!!!");
+
+
+            }
+            //copia
+            else if (rbcopia.Checked == true)
+            {
+                if (Directory.Exists(caminhodestino))
+                {
+                    if (File.Exists(caminhodestino + nomearquivo) == true)
+                    {
+                        File.Delete(caminhodestino + nomearquivo);
+                    }
+                    File.Copy(caminhoorigem + nomearquivo, caminhodestino + nomearquivo);
+                }
+                else
+                {
+                    Directory.CreateDirectory(caminhodestino);
+                    File.Copy(caminhoorigem + nomearquivo, caminhodestino + nomearquivo);
+                }
+                MessageBox.Show("Arquivo copiado com sucesso");
+            }
+            //apaga
+            else if (rbapaga.Checked == true)
+            {
+                if(File.Exists(caminhoorigem+nomearquivo) == true)
+                {
+                    File.Delete(caminhoorigem+nomearquivo);
+                    MessageBox.Show("Arquivo excluido!!!");
+
+                }
+                else
+                {
+                    MessageBox.Show("Arquivo não encontrado!!!");
+                }
+            }
+        }
+
+        private void btnlimpa_Click(object sender, EventArgs e)
+        {
+            lblista.Items.Clear();//limpar o texto
+        }
+
+        private void rbmove_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
